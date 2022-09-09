5bf0150b8d88c12d7090af394036a42bc3d6e368
Casey Hopkins <caseylouisee@me.com>
2022-09-09 14:04:10 +0100

    Seeding & Query Building

---
+++ database/factories/AnimalFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Animal>
+ */
+class AnimalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'=>fake()->name,
+            'weight'=>fake()->randomFloat(2,150,500),
+        ];
+    }
+}
